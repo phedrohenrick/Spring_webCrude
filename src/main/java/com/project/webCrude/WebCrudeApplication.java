c8c191dded8927ca10926743e6076b610a5e6d42
phedrohenrick <phedrohenrickdasilva@gmail.com>
2024-06-04 10:43:04 -0300

    projeto

---
+++ src/main/java/com/project/webCrude/WebCrudeApplication.java
@@ -0,0 +1,13 @@
+package com.project.webCrude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCrudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/webCrude/WebCrudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webCrude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
